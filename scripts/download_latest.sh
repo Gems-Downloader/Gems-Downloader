7135362caef269364594032d4f4b1ba03121b84b
Lior Spyer <lspyer@gmail.com>
2016-06-21 20:43:07 +0300

    1.0.1

--- scripts/download_latest.sh
+++ scripts/download_latest.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-sudo -u $SUDO_USER /usr/lib/postgresql/9.1/bin/pg_ctl -D ~/gems_downloader/postgresql stop
-sudo -u $SUDO_USER /usr/lib/postgresql/9.1/bin/pg_ctl -D ~/gems_downloader/postgresql start
+# stop existing user postgresql services and start a new one
+{
+  /usr/lib/postgresql/9.1/bin/pg_ctl -D ~/gems_downloader/postgresql stop
+  /usr/lib/postgresql/9.1/bin/pg_ctl -D ~/gems_downloader/postgresql start
+} &> /dev/null
 
 # update the rubygems DB
 ~/gems_downloader/rubygems.org/script/load-pg-dump -u $USER -c ~/gems_downloader/RG_PG_DUMP
--- scripts/generate_latest_manifest.sh
+++ scripts/generate_latest_manifest.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# stop existing user postgresql services and start a new one
+{
+  /usr/lib/postgresql/9.1/bin/pg_ctl -D ~/gems_downloader/postgresql stop
+  /usr/lib/postgresql/9.1/bin/pg_ctl -D ~/gems_downloader/postgresql start
+} &> /dev/null
+
 # update the rubygems DB
 ~/gems_downloader/rubygems.org/script/load-pg-dump -u $USER -c ~/gems_downloader/RG_PG_DUMP
 rm -rf ~/gems_downloader/RG_PG_DUMP
